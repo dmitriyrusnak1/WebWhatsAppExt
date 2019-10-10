255b693960cc81c4bfc3705df4f45a63caadbbf2
dmitriyrusnak1 <dmitriy.rusnak.work@gmail.com>
2019-10-10 14:10:59 +0300

    create modal window for new message

--- content-scripts/containers/SideBar/index.jsx
+++ content-scripts/containers/SideBar/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'antd/es/modal';
+import { NewMessage } from '../../components';
 import * as css from './style.css';
 
 
@@ -8,42 +10,68 @@ function SideBar() {
     const homeIcon = chrome.runtime.getURL("images/home.svg");
     const plusIcon = chrome.runtime.getURL("images/plus.svg");
 
+    const [isModalVisible, setModalVisible] = useState(false);
 
+    const [phoneNumber, setPhoneNumber] = useState(null);
+    const [message, setMessage] = useState('');
+
+    const handleChangePhone = (e) => {
+        const value = e.target.value;
+        setPhoneNumber(value);
+    }
+
+    const handleChangeMessage = (e) => {
+        const value = e.target.value;
+        setMessage(value);
+    }
+
+    const handleSendMessage = (e) => {
+        e.preventDefault();
+        setPhoneNumber(null);
+        setMessage('');
+    }
+
+    const handleOpenModal = () => {
+        setModalVisible(!isModalVisible);
+    }
+
+    const handleCancelModal = () => {
+        setModalVisible(!isModalVisible);
+        setPhoneNumber(null);
+        setMessage('');
+    }
 
     return (
         <div className='sideBar'>
             <div className={css.contentWrapper}>
                 <div className={css.iconWrapper}>
-                    <object
-                        type="image/svg+xml"
-                        data={callIcon}
-                        className={css.iconWrapper}
-                    />
+                    <img src={callIcon} alt="callIcon" />
                 </div>
                 <div>
                     <div className={css.iconWrapper}>
-                        <object
-                            type="image/svg+xml"
-                            data={homeIcon}
-                            className={css.iconWrapper}
-                        />
+                        <img src={homeIcon} alt="homeIcon" />
                     </div>
                     <div className={css.iconWrapper}>
-                        <object
-                            type="image/svg+xml"
-                            data={commentsIcon}
-                            className={css.iconWrapper}
-                        />
+                        <img src={commentsIcon} alt="commentsIcon" />
                     </div>
                 </div>
-                <div className={css.iconWrapper}>
-                    <object
-                        type="image/svg+xml"
-                        data={plusIcon}
-                        className={css.iconWrapper}
-                    />
+                <div className={css.iconWrapper} onClick={handleOpenModal}>
+                    <img src={plusIcon} alt="plusIcon" />
                 </div>
             </div>
+            <Modal
+                visible={isModalVisible}
+                footer={null}
+                onCancel={handleCancelModal}
+            >
+                <NewMessage
+                    handleChangePhone={handleChangePhone}
+                    handleChangeMessage={handleChangeMessage}
+                    handleSendMessage={handleSendMessage}
+                    phoneNumber={phoneNumber}
+                    message={message}
+                />
+            </Modal>
         </div>
     );
 }
